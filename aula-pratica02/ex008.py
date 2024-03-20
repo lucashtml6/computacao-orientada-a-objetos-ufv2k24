fb0b1d0ded0caef242f9c8ee83feecf18cbcbd7d
lucashtml6 <lucassouzarhcp9@gmail.com>
2024-03-20 20:00:47 -0300

    Adicionando comentário ao ex 08

--- aula-pratica02/ex008.py
+++ aula-pratica02/ex008.py
@@ -5,7 +5,7 @@ def insertionSort(lista):
   for i in range(1, n):
     elemAnalisado = lista[i]
     j =  i - 1
-    while j >= 0 and elemAnalisado < lista[j]:
+    while j >= 0 and elemAnalisado < lista[j]: # j>=0 => se j = -1 acabou a lista 
       lista[j+1] = lista[j]
       j -= 1
     lista[j+1] = elemAnalisado
