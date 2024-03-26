a750e732ba364e73568271ed3522ddf1ece8744f
lucashtml6 <lucassouzarhcp9@gmail.com>
2024-03-25 22:38:49 -0300

    Ex 003 INCOMPLETO

--- aula-pratica03/ex003/Pilha.py
+++ aula-pratica03/ex003/Pilha.py
@@ -2,3 +2,19 @@ class Pilha:
     __elementos = []
 
     def empilhar(self, elemento):
+        self.__elementos.append(elemento)
+    
+    def desempilhar(self):
+        if len(self.__elementos) == 0:
+            print('Pilha vazia')
+            return None
+        else:
+            ultimoElemento = self.__elementos[-1]
+            self.__elementos.pop()
+            return ultimoElemento
+
+    def getPilha(self):
+        return self.__elementos.copy()
+    
+    def lenPilha(self):
+        return len(self.__elementos)
---
+++ aula-pratica03/ex003/main.py
@@ -0,0 +1,23 @@
+from Pilha import Pilha
+
+#b
+p1 = Pilha()
+p1.empilhar(1)
+p1.empilhar(7)
+p1.empilhar(9)
+
+p2 = Pilha()
+
+#c
+# Não irá funcionar pois não é uma lista
+
+#d 
+
+p2.empilhar(4)
+p2.empilhar(10)
+pilha3 = p2.getPilha()
+
+print(pilha3)
+
+
+
